f872f9f1ec6f216848f40dcadf1f2e4931b4fabd
Juliette Cordor <grassojames5@gmail.com>
2022-03-19 14:54:35 +1100

    chore: removed build scripts

--- build.sh
+++
@@ -1,5 +0,0 @@
-cargo build --bins --release --target x86_64-unknown-linux-gnu
-cargo build --bins --release --target x86_64-pc-windows-msvc
-cargo build --bins --release --target x86_64-apple-darwin
-cargo build --bins --release --target aarch64-unknown-linux-gnu
-cargo build --bins --release --target aarch64-apple-darwin
