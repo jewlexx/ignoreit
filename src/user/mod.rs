4a2b29947d582604f8c62fa928662d344a0a55ed
Juliette Cordor <grassojames5@gmail.com>
2022-04-13 15:40:53 +1000

    updated cache path

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,12 +1,14 @@
 use directories::BaseDirs;
+use std::path::PathBuf;
 
 lazy_static! {
-    pub static ref USER_DIRS: Option<BaseDirs> = BaseDirs::new();
+    pub static ref DIRS: Option<BaseDirs> = BaseDirs::new();
     pub static ref CACHE_ENABLED: bool = {
-        if let Some(user_dirs) = USER_DIRS.to_owned() {
-            user_dirs.cache_dir().exists()
+        if let Some(dirs) = DIRS.to_owned() {
+            dirs.cache_dir().exists()
         } else {
             false
         }
     };
+    pub static ref CACHE_DIR: Option<PathBuf> = DIRS.to_owned().map(|dirs| dirs.cache_dir().into());
 }
