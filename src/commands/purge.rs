26a2e7e1a668ce30994fd6e8d102f2e5a2a38ffe
Juliette Cordor <grassojames5@gmail.com>
2022-04-13 17:13:27 +1000

    fixes

--- src/commands/purge.rs
+++ src/commands/purge.rs
@@ -5,5 +5,9 @@ use std::fs;
 pub fn purge() -> anyhow::Result<()> {
     let cache_dir = CACHE_DIR.to_owned().context("Failed to parse cache dir")?;
 
-    fs::remove_dir_all(cache_dir).with_context(|| "Failed to purge cache")
+    fs::remove_dir_all(cache_dir).with_context(|| "Failed to purge cache")?;
+
+    print!("Purged Cache!");
+
+    Ok(())
 }
