94417a603d0bb8abfe9a0f26fac9ca19ebf54a07
Juliette Cordor <grassojames5@gmail.com>
2022-04-13 18:10:06 +1000

    fixes

--- cache/src/lib.rs
+++ cache/src/lib.rs
@@ -3,6 +3,7 @@ use directories::BaseDirs;
 use git2::Repository;
 use spinners::{Spinner, Spinners};
 use std::{
+    collections::HashMap,
     fs,
     path::{Path, PathBuf},
     time::SystemTime,
@@ -60,5 +61,45 @@ pub fn init_cache() -> anyhow::Result<PathBuf> {
         Ok(cache_dir)
     } else {
         Err(anyhow::anyhow!("Cache directory not found"))
+    }
+}
+
+pub fn get_templates() -> anyhow::Result<HashMap<String, String>> {
+    if let Some(cache_dir) = CACHE_DIR.to_owned() {
+        let dir = fs::read_dir(cache_dir).with_context(|| "Failed to read cache directory")?;
+
+        let ignores_tuple = dir
+            .filter(|e| {
+                if let Ok(entry) = e {
+                    entry.file_type().unwrap().is_file()
+                        && entry.file_name().to_str().unwrap().ends_with(".gitignore")
+                } else {
+                    false
+                }
+            })
+            .map(|e| -> anyhow::Result<(String, String)> {
+                let entry = e?;
+                let file_name = entry.file_name();
+                let name = file_name
+                    .to_str()
+                    .context("Failed to parse file name")?
+                    .split('.')
+                    .next()
+                    .context("Failed to parse file name")?
+                    .to_owned();
+
+                Ok((name.to_lowercase(), name))
+            })
+            .collect::<anyhow::Result<Vec<_>>>()?;
+
+        let mut ignores = HashMap::<String, String>::new();
+
+        for (lower, path) in ignores_tuple {
+            ignores.insert(lower, path);
+        }
+
+        Ok(ignores)
+    } else {
+        remote::get_templates()
     }
 }
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -2,45 +2,4 @@
 pub use remote::get_templates;
 
 #[cfg(feature = "cache")]
-pub fn get_templates() -> anyhow::Result<std::collections::HashMap<String, String>> {
-    use anyhow::Context;
-    use std::{collections::HashMap, fs};
-
-    if let Some(cache_dir) = cache::CACHE_DIR.to_owned() {
-        let dir = fs::read_dir(cache_dir).with_context(|| "Failed to read cache directory")?;
-
-        let ignores_tuple = dir
-            .filter(|e| {
-                if let Ok(entry) = e {
-                    entry.file_type().unwrap().is_file()
-                        && entry.file_name().to_str().unwrap().ends_with(".gitignore")
-                } else {
-                    false
-                }
-            })
-            .map(|e| -> anyhow::Result<(String, String)> {
-                let entry = e?;
-                let file_name = entry.file_name();
-                let name = file_name
-                    .to_str()
-                    .context("Failed to parse file name")?
-                    .split('.')
-                    .next()
-                    .context("Failed to parse file name")?
-                    .to_owned();
-
-                Ok((name.to_lowercase(), name))
-            })
-            .collect::<anyhow::Result<Vec<_>>>()?;
-
-        let mut ignores = HashMap::<String, String>::new();
-
-        for (lower, path) in ignores_tuple {
-            ignores.insert(lower, path);
-        }
-
-        Ok(ignores)
-    } else {
-        remote::get_templates()
-    }
-}
+pub use cache::get_templates;
