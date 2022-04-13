096c08679c872f2d6129384bf2c39e5a5972758f
Juliette Cordor <grassojames5@gmail.com>
2022-04-13 17:59:36 +1000

    addded a line in between enum children for readability

--- src/commands/args.rs
+++ src/commands/args.rs
@@ -11,11 +11,13 @@ pub struct Args {
 pub enum Commands {
     #[clap(about = "List all available templates")]
     List,
+
     #[clap(about = "Pull a template from the repository", args = [
         Arg::new("template").takes_value(true).required(true),
         Arg::new("output").takes_value(true).required(false),
     ])]
     Pull,
+
     #[cfg(feature = "cache")]
     #[clap(about = "Purge gitignore cache")]
     Purge,
