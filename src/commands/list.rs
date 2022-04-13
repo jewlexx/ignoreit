e36082b974016b9b7dec7bb14c078a499ac6c33b
Juliette Cordor <grassojames5@gmail.com>
2022-04-13 16:38:38 +1000

    fixes

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,11 +1,7 @@
-use spinners::{Spinner, Spinners};
-
 use crate::lib::get_templates;
 
 pub fn list_templates() -> anyhow::Result<()> {
-    let sp = Spinner::new(Spinners::Dots12, "Fetching templates...".into());
     let map = get_templates()?;
-    sp.stop();
 
     println!("Available templates:");
 
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,9 +1,9 @@
 use anyhow::Context as _;
 use reqwest::{blocking::Response, header::USER_AGENT};
 use serde_json::Value;
-use std::collections::HashMap;
+use std::{collections::HashMap, fs};
 
-const TEMPLATES_URL: &str = "https://api.github.com/repos/github/gitignore/git/trees/main";
+use crate::cache::CACHE_DIR;
 
 #[macro_use]
 pub mod macros;
@@ -25,7 +25,40 @@ pub fn get_url(str: &str) -> anyhow::Result<Response> {
 }
 
 pub fn get_templates() -> anyhow::Result<HashMap<String, String>> {
-    let body: Value = get_url(TEMPLATES_URL)?
+    if let Some(cache_dir) = CACHE_DIR.to_owned() {
+        let dir = fs::read_dir(cache_dir).with_context(|| "Failed to read cache directory")?;
+
+        let ignores_tuple = dir
+            .filter(|e| {
+                if let Ok(entry) = e {
+                    entry.file_type().unwrap().is_file()
+                        && entry.file_name().to_str().unwrap().ends_with(".gitignore")
+                } else {
+                    false
+                }
+            })
+            .map(|e| {
+                let entry = e.unwrap();
+                let file_name = entry.file_name();
+                let name = file_name.to_str().unwrap();
+                let path = entry.path();
+
+                let contents = String::from_utf8(fs::read(path).unwrap()).unwrap();
+
+                (name.to_owned(), contents)
+            })
+            .collect::<Vec<(String, String)>>();
+
+        let mut ignores = HashMap::<String, String>::new();
+
+        for (name, contents) in ignores_tuple {
+            ignores.insert(name, contents);
+        }
+
+        return Ok(ignores);
+    }
+
+    let body: Value = get_url("https://api.github.com/repos/github/gitignore/git/trees/main")?
         .json()
         .with_context(|| "Failed to read JSON from response")?;
 
