b2a6dacc1451f89023db71ec87f1f93127ac4ff9
Juliette Cordor <grassojames5@gmail.com>
2022-05-02 18:10:19 +1000

    fixed caching

--- src/commands/pull.rs
+++ src/commands/pull.rs
@@ -2,31 +2,15 @@ use std::{
     env,
     fs::File,
     io::{self, Write},
-    time::SystemTime,
 };
 
 use anyhow::Context;
 
-use crate::{
-    flush_stdout,
-    lib::{get_templates, CACHE_DIR, IS_ONLINE},
-};
+use crate::{flush_stdout, lib::get_templates};
 
 pub fn get_contents_remote(template_path: &str) -> anyhow::Result<Vec<u8>> {
     let url = crate::parse_url!(template_path);
     let contents = crate::remote::get_url(&url)?.text()?.as_bytes().to_vec();
-
-    #[cfg(feature = "cache")]
-    {
-        let cache_file = CACHE_DIR
-            .to_owned()
-            .context("Failed to find cache dir")?
-            .join(format!("{}.gitignore", template_path));
-
-        let mut file = File::create(cache_file)?;
-
-        file.write_all(&contents)?;
-    }
 
     Ok(contents)
 }
@@ -72,21 +56,8 @@ pub fn pull_template() -> anyhow::Result<()> {
         use crate::lib::CACHE_ENABLED;
 
         if CACHE_ENABLED.to_owned() {
-            let cache_dir = CACHE_DIR.to_owned().context("Failed to parse cache dir")?;
-            let file_path = cache_dir.join(format!("{}.gitignore", template_path));
-            let last_modified = file_path.metadata()?.modified()?;
-
-            if SystemTime::now().duration_since(last_modified)?.as_secs() * 60 * 60 > 24
-                && IS_ONLINE.to_owned()
-            {
-                println!("Getting template {}", template_path);
-                get_contents_remote(template_path)?
-            } else {
-                match std::fs::read(file_path) {
-                    Ok(v) => v,
-                    Err(_) => get_contents_remote(template_path)?,
-                }
-            }
+            println!("Getting template {}", template_path);
+            get_contents_remote(template_path)?
         } else {
             Vec::new()
         }
