4c002cfea008f4ac1750b585bfa8ea584b1bbcb7
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 09:02:51 +1000

    fixes

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -58,41 +58,38 @@ pub fn init_cache() -> anyhow::Result<PathBuf> {
 }
 
 pub fn get_templates() -> anyhow::Result<HashMap<String, String>> {
-    if let Some(cache_dir) = CACHE_DIR.to_owned() {
-        let dir = fs::read_dir(cache_dir).with_context(|| "Failed to read cache directory")?;
+    let cache_dir = CACHE_DIR.to_owned().context("Cache directory not found")?;
+    let dir = fs::read_dir(cache_dir).with_context(|| "Failed to read cache directory")?;
 
-        let ignores_tuple = dir
-            .filter(|e| {
-                if let Ok(entry) = e {
-                    entry.file_type().unwrap().is_file()
-                        && entry.file_name().to_str().unwrap().ends_with(".gitignore")
-                } else {
-                    false
-                }
-            })
-            .map(|e| -> anyhow::Result<(String, String)> {
-                let entry = e?;
-                let file_name = entry.file_name();
-                let name = file_name
-                    .to_str()
-                    .context("Failed to parse file name")?
-                    .split('.')
-                    .next()
-                    .context("Failed to parse file name")?
-                    .to_owned();
+    let ignores_tuple = dir
+        .filter(|e| {
+            if let Ok(entry) = e {
+                entry.file_type().unwrap().is_file()
+                    && entry.file_name().to_str().unwrap().ends_with(".gitignore")
+            } else {
+                false
+            }
+        })
+        .map(|e| -> anyhow::Result<(String, String)> {
+            let entry = e?;
+            let file_name = entry.file_name();
+            let name = file_name
+                .to_str()
+                .context("Failed to parse file name")?
+                .split('.')
+                .next()
+                .context("Failed to parse file name")?
+                .to_owned();
 
-                Ok((name.to_lowercase(), name))
-            })
-            .collect::<anyhow::Result<Vec<_>>>()?;
+            Ok((name.to_lowercase(), name))
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?;
 
-        let mut ignores = HashMap::<String, String>::new();
+    let mut ignores = HashMap::<String, String>::new();
 
-        for (lower, path) in ignores_tuple {
-            ignores.insert(lower, path);
-        }
-
-        Ok(ignores)
-    } else {
-        crate::remote::get_templates()
+    for (lower, path) in ignores_tuple {
+        ignores.insert(lower, path);
     }
+
+    Ok(ignores)
 }
