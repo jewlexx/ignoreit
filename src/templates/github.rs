a607438d8507c3b1d5ed1b05d979d1db11fca24c
Juliette Cordor <grassojames5@gmail.com>
2022-08-03 11:54:19 +1000

    fix:

--- src/templates/github.rs
+++ src/templates/github.rs
@@ -21,8 +21,17 @@ impl GithubApi {
         let mapped: Vec<(String, Vec<u8>)> = response
             .par_iter()
             .progress_count(response.len() as u64)
-            .map(|template| {})
-            .collect();
+            .map(|template| {
+                let download_url = format!(
+                    "https://raw.githubusercontent.com/github/gitignore/main/{}.gitignore",
+                    template
+                );
+
+                let file = reqwest::blocking::get(download_url)?;
+
+                Ok((template.to_string(), vec![9]))
+            })
+            .collect::<Vec<Result<_, _>>>();
 
         Ok(Self { response })
     }
