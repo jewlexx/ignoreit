26efc380d57f3c2d81d1c8d1f3d9934a181600e3
Juliette Cordor <grassojames5@gmail.com>
2022-08-03 12:44:47 +1000

    feat: added timestamp static

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![forbid(unsafe_code)]
+use std::time::SystemTime;
+
 use clap::Parser;
 
 mod cache;
@@ -9,7 +11,13 @@ mod utils;
 
 use commands::args::Args;
 
+lazy_static::lazy_static! {
+    static ref TIMESTAMP: u128 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("time went backwards").as_millis();
+}
+
 fn main() -> anyhow::Result<()> {
+    lazy_static::initialize(&TIMESTAMP);
+
     if *utils::CACHE_ENABLED {
         cache::init_cache()?;
     } else {
