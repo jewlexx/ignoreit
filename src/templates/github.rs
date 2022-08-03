1859542ba19258a2dc5a54ec7ee3018372e1acdd
Juliette Cordor <grassojames5@gmail.com>
2022-08-03 11:41:02 +1000

    feat:

--- src/templates/github.rs
+++ src/templates/github.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
 pub type Gitignores = Vec<String>;
 
-pub struct GithubApi {}
+pub struct GithubApi {
+    response: Gitignores,
+}
+
+impl GithubApi {
+    // TODO: Implement a better error type
+    pub fn new() -> anyhow::Result<Self> {
+        const API_URL: &str = "https://api.github.com/gitignore/templates";
+
+        let response: Gitignores = reqwest::blocking::get(API_URL)?.json()?;
+
+        Ok(Self { response })
+    }
+}
