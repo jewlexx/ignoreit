fb3c7a0d22596c5738410bc98c1dfb341f6f3a11
Juliette Cordor <professional@maybejules.com>
2024-07-07 13:09:16 +0200

    added boilerplate async main fn

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,15 @@
-//! CLI application to pull gitignore templates with ease
-
-#![forbid(unsafe_code)]
-#![warn(missing_docs)]
-
-use std::time::SystemTime;
-
-use clap::Parser;
-
-use lazy_static::lazy_static;
-
-lazy_static! {
-    /// The current time in milliseconds
-    pub static ref TIMESTAMP: u128 = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .expect("time went backwards")
-        .as_millis();
+fn main() {
+    if let Err(e) = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Correctly initialized asynchronous runtime")
+        .block_on(_main())
+    {
+        // TODO: Better error handling
+        panic!("{:?}", e)
+    }
 }
 
-pub mod cache;
-pub mod commands;
-pub mod macros;
-pub mod templates;
-
-// TODO: add custom errors with `thiserror`
-
-use commands::args::Args;
-
-fn main() -> anyhow::Result<()> {
-    lazy_static::initialize(&TIMESTAMP);
-
-    if !*cache::CACHE_ENABLED {
-        use mincolor::Colorize;
-        println!(
-            "{}",
-            "warning: cache is disabled. performance will not be optimal".yellow()
-        );
-        sleep_for!(3000);
-    }
-
-    let args = Args::parse();
-
-    args.command.run()?;
-
+async fn _main() -> anyhow::Result<()> {
     Ok(())
 }
