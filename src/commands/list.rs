cc6cb4c9869a4442a581ae58373f393ea5828057
Juliette Cordor <professional@maybejules.com>
2024-07-29 18:09:21 +1000

    added template listing

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,9 +1,15 @@
+use crate::CACHE;
+
 #[derive(Debug, Clone, clap::Parser)]
 pub struct Args;
 
 impl super::Command for Args {
     async fn run(&self) -> anyhow::Result<()> {
-        println!("Listing templates...");
+        println!("Available Templates:");
+
+        for template in CACHE.list_templates() {
+            println!("\t{}", template.name());
+        }
 
         Ok(())
     }
