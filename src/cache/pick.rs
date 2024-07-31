ba70f9011194c88de72e94f604bcc609eb6cbb94
Juliette Cordor <professional@maybejules.com>
2024-07-31 12:22:56 +1000

    put all state in the same struct

--- src/cache/pick.rs
+++ src/cache/pick.rs
@@ -1,7 +1,8 @@
-use std::{cmp::Ordering, io::stdout, sync::Mutex};
+use std::{cmp::Ordering, io::stdout};
 
 use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use itertools::Itertools;
+use parking_lot::Mutex;
 use ratatui::{
     crossterm::{
         event::{self, Event, KeyCode, KeyEventKind, KeyModifiers},
@@ -36,21 +37,26 @@ fn indices_template<'a>(template: &Template, indices: &[usize]) -> Vec<Span<'a>>
     spans
 }
 
+struct State {
+    matching_templates: Vec<(Template, Vec<usize>)>,
+    search_term: String,
+    list_state: ListState,
+}
+
 pub fn pick_template(templates: &[Template]) -> anyhow::Result<Option<Template>> {
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
-    let list_state = Mutex::new({
-        let mut state = ListState::default();
-        state.select(Some(0));
-        state
+    let state = Mutex::new(State {
+        matching_templates: templates.iter().map(|t| (t.clone(), vec![])).collect(),
+        search_term: String::new(),
+        list_state: {
+            let mut state = ListState::default();
+            state.select(Some(0));
+            state
+        },
     });
-
-    let search_term = Mutex::new(String::new());
-
-    let matching_templates: Mutex<Vec<(Template, Vec<usize>)>> =
-        Mutex::new(templates.iter().map(|t| (t.clone(), vec![])).collect());
 
     let ui = |frame: &mut Frame| {
         let chunks = Layout::default()
@@ -58,21 +64,15 @@ pub fn pick_template(templates: &[Template]) -> anyhow::Result<Option<Template>>
             .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
             .split(frame.size());
 
-        let text_input = Paragraph::new(search_term.lock().unwrap().clone().cyan())
+        let text_input = Paragraph::new(state.lock().search_term.clone().cyan())
             .block(Block::bordered().title("Fuzzy Search"))
             .style(Style::default().fg(Color::White));
 
         frame.render_widget(text_input, chunks[0]);
 
-        let list = List::new(
-            matching_templates
-                .lock()
-                .unwrap()
-                .iter()
-                .map(|(t, indices)| {
-                    Line::from(indices_template(t, indices)).add_modifier(Modifier::DIM)
-                }),
-        )
+        let list = List::new(state.lock().matching_templates.iter().map(|(t, indices)| {
+            Line::from(indices_template(t, indices)).add_modifier(Modifier::DIM)
+        }))
         .block(
             Block::bordered()
                 .title("Templates")
@@ -82,13 +82,12 @@ pub fn pick_template(templates: &[Template]) -> anyhow::Result<Option<Template>>
         .highlight_style(Style::default().remove_modifier(Modifier::DIM))
         .highlight_symbol("> ");
 
-        frame.render_stateful_widget(list, chunks[1], &mut *list_state.lock().unwrap());
+        frame.render_stateful_widget(list, chunks[1], &mut state.lock().list_state);
     };
 
     let selected = loop {
         terminal.draw(ui)?;
-        let (should_quit, selected) =
-            handle_events(templates, &matching_templates, &search_term, &list_state)?;
+        let (should_quit, selected) = handle_events(templates, &state)?;
 
         if should_quit {
             break selected;
@@ -103,19 +102,19 @@ pub fn pick_template(templates: &[Template]) -> anyhow::Result<Option<Template>>
 
 fn handle_events(
     templates: &[Template],
-    matching_templates: &Mutex<Vec<(Template, Vec<usize>)>>,
-    search_term: &Mutex<String>,
-    list_state: &Mutex<ListState>,
+    state: &Mutex<State>,
 ) -> anyhow::Result<(bool, Option<Template>)> {
+    let mut state = state.lock();
+
     let mut should_quit = false;
 
-    let current_index = list_state.lock().unwrap().selected().unwrap_or(0);
+    let current_index = state.list_state.selected().unwrap_or(0);
 
-    *matching_templates.lock().unwrap() = templates
+    state.matching_templates = templates
         .iter()
         .filter_map(|t| {
             SkimMatcherV2::default()
-                .fuzzy_indices(t.name(), search_term.lock().unwrap().as_str())
+                .fuzzy_indices(t.name(), state.search_term.as_str())
                 .map(|(score, indices)| (t, score, indices))
         })
         .sorted_by(
@@ -133,12 +132,12 @@ fn handle_events(
         Down,
     }
 
-    let adjust_index = |index: usize, adjustment: Adjustment| {
-        let templates_len = {
-            let matching_templates = matching_templates.lock().unwrap();
-            matching_templates.len()
-        };
+    let templates_len = {
+        let matching_templates = &state.matching_templates;
+        matching_templates.len()
+    };
 
+    let adjust_index = |index: usize, adjustment: Adjustment| {
         if index == 0 && adjustment == Adjustment::Down {
             templates_len - 1
         } else if index == templates_len - 1 && adjustment == Adjustment::Up {
@@ -157,30 +156,28 @@ fn handle_events(
                 KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => {
                     should_quit = true
                 }
-                KeyCode::Up => list_state
-                    .lock()
-                    .unwrap()
+                KeyCode::Up => state
+                    .list_state
                     .select(Some(adjust_index(current_index, Adjustment::Down))),
-                KeyCode::Down => list_state
-                    .lock()
-                    .unwrap()
+                KeyCode::Down => state
+                    .list_state
                     .select(Some(adjust_index(current_index, Adjustment::Up))),
                 KeyCode::Enter => {
-                    let selected = list_state.lock().unwrap().selected();
+                    let selected = state.list_state.selected();
 
                     if let Some(selected) = selected {
                         return Ok((true, Some(templates[selected].clone())));
                     }
                 }
                 KeyCode::Backspace => {
-                    let mut search_term = search_term.lock().unwrap();
+                    let search_term = &mut state.search_term;
 
                     if !search_term.is_empty() {
                         search_term.pop();
                     }
                 }
                 KeyCode::Char(c) => {
-                    let mut search_term = search_term.lock().unwrap();
+                    let search_term = &mut state.search_term;
 
                     search_term.push(c);
                 }
