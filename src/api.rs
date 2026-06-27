3a1c39db0e55a37b70f5c3d4816f999614f23aac
Juliette Cordor <me@cordor.dev>
2026-06-27 14:10:58 +1000

    fixed api types

--- src/api.rs
+++ src/api.rs
@@ -11,4 +11,22 @@ pub struct Gitignore {
     pub(crate) contents: String,
     pub(crate) file_name: String,
     pub(crate) key: String,
+    #[sqlx(try_from = "i64")]
+    pub(crate) is_patch: IsPatch,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Hash)]
+pub enum IsPatch {
+    True,
+    False,
+}
+
+impl From<i64> for IsPatch {
+    fn from(value: i64) -> Self {
+        match value {
+            0 => Self::False,
+            1 => Self::True,
+            _ => unreachable!(),
+        }
+    }
 }
